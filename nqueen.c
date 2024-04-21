de0fc9349cb5ebe5448062926b900e7bdd8cd9a3
Absar Gadoo <106620103+Absargadoo@users.noreply.github.com>
2024-04-21 20:52:11 +0530

    Add files via upload

---
+++ nqueen.c
@@ -0,0 +1,19 @@
+//alogorithm
+Nqueens(k,n)
+{
+    for(i=1;i<=n;i+=)
+    {
+        if(place(k,n));
+        x[k]=i;
+        (k==n);
+        print x([1:n])
+    }
+    else
+    nqueen(k+1,n)
+    {
+        place(k,n);
+        if(x[k]=i) || abs(x[j]-i=abs(j-k))
+        return false;
+    }
+return true;
+}
